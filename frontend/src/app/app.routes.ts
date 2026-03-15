b43ecc668f2c1e54a4ca09a38dbfac659204793b
Sakib Khan <sakibhkhan11@gmail.com>
2026-03-15 14:41:21 -0400

    added signup page and login validation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
+import { SignupComponent } from './signup/signup';
 import { HomeComponent } from './home/home';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent }
 ];
