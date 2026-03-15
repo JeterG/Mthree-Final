8de00d255b4aa3842ae8308c2bfbfba33932b0a9
JeterG <jetergdev@gmail.com>
2026-03-15 16:29:36 -0400

    Add the common module to the signup.ts

--- frontend/src/app/signup/signup.ts
+++ frontend/src/app/signup/signup.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
@@ -5,18 +6,16 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './signup.html',
-  styleUrl: './signup.css'
+  styleUrl: './signup.css',
 })
 export class SignupComponent {
-
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
 
-  signup(){
-    console.log("Signup:", this.email, this.password);
+  signup() {
+    console.log('Signup:', this.email, this.password);
   }
-
 }
