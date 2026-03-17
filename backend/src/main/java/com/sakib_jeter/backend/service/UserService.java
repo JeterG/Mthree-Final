cf88c8e0c7eef38503f202085c93194ebe5d82da
JeterG <jetergdev@gmail.com>
2026-03-17 07:48:14 -0400

    - Implement UserService - Inject UserRepository - getUserById - getAllUsers - addUser - deleteUser - updateUser

--- backend/src/main/java/com/sakib_jeter/backend/service/UserService.java
+++ backend/src/main/java/com/sakib_jeter/backend/service/UserService.java
@@ -1,5 +1,46 @@
 package com.sakib_jeter.backend.service;
 
+import java.util.List;
+
+import com.sakib_jeter.backend.entity.User;
+import com.sakib_jeter.backend.repository.UserRepository;
+
 public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email Exists");
+        }
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        if (userRepository.findById(id) != null) {
+            userRepository.deleteById(id);
+        }
+    }
+
+    public User updateUser(Long id, User user) {
+        User currentUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+
+        currentUser.setEmail(user.getEmail());
+        currentUser.setLastLogin(user.getLastLogin());
+
+        return userRepository.save(currentUser);
+    }
 
 }
