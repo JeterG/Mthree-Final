911d16182bde685cfb6737197e923ea0e61101bf
JeterG <jetergdev@gmail.com>
2026-03-17 19:52:50 -0400

    Implement TransactionRepository.java - create the findByUserIdOrderByCreateAtDesc, which is the equaivalent of select * from transactions where user_id = id order by created_at DESC

--- backend/src/main/java/com/sakib_jeter/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/sakib_jeter/backend/repository/TransactionRepository.java
@@ -1,5 +1,12 @@
 package com.sakib_jeter.backend.repository;
 
-public interface TransactionRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sakib_jeter.backend.entity.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByUserIdOrderByCreatedAtDesc(Long userId); // Get all transactions based on user_id sorted by
+                                                                     // most recent first
 }
