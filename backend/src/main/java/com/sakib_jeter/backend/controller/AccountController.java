6f3256e440cb0474a6ffef6c32b5ebecb1244db3
JeterG <jetergdev@gmail.com>
2026-03-17 22:34:19 -0400

    Account Backend Workflow - Implement Account.java and add fields to financeDb.sql so that we can also generate the tables manually, and defined as an entity to generate them automatically on springboot launch - Implement AccountRepository.java - use jpa query methods for findByUserId - IMplement AccountService.java , get al laccounts, get account by user id, create account, delete account, update account - Implement AccountController.java at point /account however the following are the endpoints to not expose the account id since the user id is what is used in other places more securely - GET /account/{userId} --> To get the account - GET /account --> get all accounts - POST /account --> create account with the given body - PUT /account/{userId} --> update the account by searching using the userid and the updating fields being in the body of the account object. - add declaration to financeDb.sql

--- backend/src/main/java/com/sakib_jeter/backend/controller/AccountController.java
+++ backend/src/main/java/com/sakib_jeter/backend/controller/AccountController.java
@@ -1,5 +1,51 @@
 package com.sakib_jeter.backend.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sakib_jeter.backend.entity.Account;
+import com.sakib_jeter.backend.service.AccountService;
+
+@RestController
+@RequestMapping("/account")
 public class AccountController {
 
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/{userId}") // user userId instead of account id to not expose account id
+    public Account getAccount(@PathVariable Long userId) {
+        return accountService.getAccountByUserId(userId);
+    }
+
+    @PostMapping
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+
+    @PutMapping("/{userId}") // user userId instead of account id to not expose account id
+    public Account updateAccount(@PathVariable Long userId, @RequestBody Account updatedAccount) {
+        return accountService.updateAccount(userId, updatedAccount);
+    }
+
+    @DeleteMapping("/{userId}") // user userId instead of account id to not expose account id
+    public void deleteAccount(@PathVariable Long userId) {
+        accountService.deleteAccount(userId);
+    }
 }
--- backend/src/main/java/com/sakib_jeter/backend/service/AccountService.java
+++ backend/src/main/java/com/sakib_jeter/backend/service/AccountService.java
@@ -1,5 +1,45 @@
 package com.sakib_jeter.backend.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sakib_jeter.backend.entity.Account;
+import com.sakib_jeter.backend.repository.AccountRepository;
+
+@Service
 public class AccountService {
 
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Account getAccountByUserId(Long userId) {
+        Account account = accountRepository.findByUserId(userId);
+        if (account == null)
+            throw new RuntimeException("Account not found");
+        return account;
+    }
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Account updateAccount(Long userId, Account updatedAccount) {
+        Account account = getAccountByUserId(userId);
+        account.setFirstName(updatedAccount.getFirstName());
+        account.setLastName(updatedAccount.getLastName());
+        return accountRepository.save(account);
+    }
+
+    public void deleteAccount(Long userId) {
+        Account account = getAccountByUserId(userId);
+        accountRepository.delete(account);
+    }
 }
