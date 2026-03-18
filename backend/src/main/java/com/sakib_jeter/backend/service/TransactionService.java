94c4bf5eaa5c94d0d0e0477c82c5e0e350459540
JeterG <jetergdev@gmail.com>
2026-03-17 21:37:05 -0400

    - Implement the Transaction Controller - Implement the Transaction Service -GET / transactions endpoint for getting all transactions -POST /transactions to store a new transaction objects - NO PUT MAPPING transactions should never be updated. - Inject Transaction Repository into transaction service -Inject transaction service into transaction controller

--- backend/src/main/java/com/sakib_jeter/backend/service/TransactionService.java
+++ backend/src/main/java/com/sakib_jeter/backend/service/TransactionService.java
@@ -1,5 +1,26 @@
 package com.sakib_jeter.backend.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sakib_jeter.backend.entity.Transaction;
+import com.sakib_jeter.backend.repository.TransactionRepository;
+
+@Service
 public class TransactionService {
 
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public List<Transaction> getTransactionsByUser(Long userId) {// Get all transaction ordered by most recent
+        return transactionRepository.findByUserIdOrderByCreatedAtDesc(userId);
+    }
+
+    public Transaction saveTransaction(Transaction transaction) {// save a new transaction
+        return transactionRepository.save(transaction);
+    }
 }
