da0610b7fbe9241c857b9d58af2a554701c9f234
Sakib Khan <sakibhkhan11@gmail.com>
2026-03-19 15:28:39 -0400

    added search bar with functionality to search page and changed /market to /search

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { LoginComponent } from './login/login';
 import { Portfolio } from './portfolio/portfolio';
-import { MarketIndexComponent } from './search/market-index';
 import { SignupComponent } from './signup/signup';
+import { Search } from './search/search'; // ✅ ADD THIS
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'market', component: MarketIndexComponent },
+  { path: 'search', component: Search }, // ✅ FIXED
   { path: 'portfolio', component: Portfolio },
 ];
