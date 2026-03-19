e739e28d9216ab4b757d634afdda7914d5ee226c
JeterG <jetergdev@gmail.com>
2026-03-19 05:39:40 -0400

    - Add portfolio component temp files - add placeholder for the portfolio components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login';
-import { SignupComponent } from './signup/signup';
 import { HomeComponent } from './home/home';
+import { LoginComponent } from './login/login';
+import { Portfolio } from './portfolio/portfolio';
 import { MarketIndexComponent } from './search/market-index';
+import { SignupComponent } from './signup/signup';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
-  {path: 'market',component: MarketIndexComponent}
+  { path: 'market', component: MarketIndexComponent },
+  { path: 'portfolio', component: Portfolio },
 ];
