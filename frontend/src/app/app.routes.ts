6f6e5ab20c5817a2ee429358e9a46dba2bbf1f57
Sakib Khan <sakibhkhan11@gmail.com>
2026-03-19 00:52:13 -0400

    exchange chart added with filters

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { SignupComponent } from './signup/signup';
 import { HomeComponent } from './home/home';
+import { MarketIndexComponent } from './search/market-index';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  {path: 'market',component: MarketIndexComponent}
 ];
