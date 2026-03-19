1a7a203545e4130d52ac80ec6314fa2196889c20
Sakib Khan <sakibhkhan11@gmail.com>
2026-03-19 12:39:26 -0400

    added logout button

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,15 +1,31 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { Navbar } from './navbar/navbar';
-
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('frontend');
+  showNavbar = true;
+
+  constructor(private router: Router) {
+    this.updateNavbar(this.router.url);
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.updateNavbar(event.urlAfterRedirects);
+      });
+  }
+
+  private updateNavbar(url: string) {
+    this.showNavbar = !(url.includes('login') || url.includes('signup'));
+  }
 }
