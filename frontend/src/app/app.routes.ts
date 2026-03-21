864f2245448b391415742b1879631acfc41b133a
JeterG <jetergdev@gmail.com>
2026-03-21 13:17:59 -0400

    - update diagrams to show up in the main website

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,25 @@
 import { Routes } from '@angular/router';
+import { DiagramsComponent } from './assets/diagrams/diagrams';
 import { HomeComponent } from './home/home';
 import { LoginComponent } from './login/login';
 import { Portfolio } from './portfolio/portfolio';
+import { Search } from './search/search';
+import { SettingsComponent } from './settings/settings';
 import { SignupComponent } from './signup/signup';
 import { SwaggerComponent } from './swagger/swagger';
-import { Search } from './search/search'; 
-import { SettingsComponent } from './settings/settings';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'search', component: Search }, // 
+  { path: 'search', component: Search },
   { path: 'portfolio', component: Portfolio },
-  { path: 'docs', component: SwaggerComponent }, // ✅
-  {path: 'stock/:symbol', loadComponent: () => import('./stock-page/stock-page').then(m => m.StockPageComponent)},
-  {path: 'settings', component: SettingsComponent}
+  { path: 'docs', component: SwaggerComponent },
+  { path: 'diagrams', component: DiagramsComponent },
+  {
+    path: 'stock/:symbol',
+    loadComponent: () => import('./stock-page/stock-page').then((m) => m.StockPageComponent),
+  },
+  { path: 'settings', component: SettingsComponent },
 ];
