9f2ca6d82f68a176a01e2e4bca89a9365ee3ffe0
JeterG <jetergdev@gmail.com>
2026-03-23 11:08:18 -0400

    fix the market cap calculation

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://leu-mounts-blanket-occasionally.trycloudflare.com',
+  apiUrl: 'https://resolve-exciting-subscribers-hollywood.trycloudflare.com',
 };
