255d06aaba9e199d1d69771a72de60840874d47c
Sakib Khan <sakibhkhan11@gmail.com>
2026-03-25 23:37:07 -0400

    more comment stuff

--- frontend/src/app/search/search.ts
+++ frontend/src/app/search/search.ts
@@ -13,10 +13,10 @@ import { StockSearchComponent } from '../stock-search-component/stock-search-com
 })
 export class Search {
   constructor(private router: Router) {}
-
+  //4 most popular companies hardcoded 
   symbols = ['AAPL', 'NVDA', 'MSFT', 'TSLA'];
   selectedSymbol: string = 'AAPL';
-
+  //filters for chart
   ranges: string[] = ['1D', '1W', '1M', '3M', '1Y'];
   selectedRange: string = '1D';
 
